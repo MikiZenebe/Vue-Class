67a72288005285603358cf442c6cdd72418f87cf
MikiZenebe <mickyaszenebe@gmail.com>
2024-03-22 00:43:30 -0500

    computed

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import Binding from "@/components/Binding.vue";
 import ConditionalRender from "@/components/ConditionalRender.vue";
 import ListRendering from "@/components/ListRendering.vue";
 import Methods from "@/components/Methods.vue";
-import EventHandler from '@/components/EventHandler.vue'
+import EventHandler from "@/components/EventHandler.vue";
 import FormHandling from "./components/FormHandling.vue";
+import Computed from "@/components/Computed.vue";
 
 export default {
   name: "App",
@@ -14,7 +15,8 @@ export default {
     ListRendering,
     Methods,
     EventHandler,
-    FormHandling
+    FormHandling,
+    Computed
   },
 };
 </script>
@@ -25,14 +27,13 @@ export default {
   <!-- <ListRendering /> -->
   <!-- <Methods /> -->
   <!-- <EventHandler /> -->
-  <FormHandling />
+  <!-- <FormHandling /> -->
+  <Computed />
 </template>
 ;
 
-
 <style>
-
-  #app{
-   text-align: center;
-  }
+#app {
+  text-align: center;
+}
 </style>
---
+++ src/components/Computed.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h3>Computed fullname - {{ fullName }}</h3>
+  </div>
+
+  <button @click="items.push({ id: 4, title: 'Infinix Hot', price: 10000 })">
+    Add Item
+  </button>
+
+  <div>
+    <h3>Computed Total - {{ total }}</h3>
+  </div>
+
+  <hr />
+  <template v-for="item in items" :key="item.id">
+    <h3 v-if="item.price > 6000">{{ item.title }} {{ item.price }}</h3>
+    <h3 v-for="item in expensiveItems" :key="item.id">
+      Computed Expensive {{ item.title }} {{ item.price }}
+    </h3>
+  </template>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "Mickyas",
+      lastName: "Zenebe",
+      items: [
+        {
+          id: 1,
+          title: "Samsung A10",
+          price: 5500,
+        },
+        {
+          id: 2,
+          title: "Samsung A30",
+          price: 9500,
+        },
+        {
+          id: 3,
+          title: "Samsung A70",
+          price: 20000,
+        },
+      ],
+    };
+  },
+
+  methods: {},
+
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    total() {
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
+    },
+    expensiveItems() {
+      return this.items.filter((item) => item.price > 7000);
+    },
+  },
+};
+</script>
